b0517d4d9629201f5d9cc79921918ddeacc67b71
Al Cummings <akckc@users.noreply.github.com>
2019-09-12 14:30:46 -0500

    added the name page

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app=Flask(__name__)
 @app.route('/')
 def hello():
     return "hello wurld"
-
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!.".format(name)
 if __name__=='__main__':
     app.run()
